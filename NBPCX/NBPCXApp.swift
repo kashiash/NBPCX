3e4ab6071d328912cc59db6fd252ffcb04c6dbcf
Jacek Kosinski <kashiash@gmail.com>
2022-11-28 23:14:51 +0100

    Initial Commit

---
+++ NBPCX/NBPCXApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBPCXApp.swift
+//  NBPCX
+//
+//  Created by Jacek Kosiński G on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NBPCXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
